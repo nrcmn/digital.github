3b1bff79fc64d19dc40c2c46d5aa44ddfb28e8bc
Carl-fridrix-zendolf-IV <social.neet@gmail.com>
2015-04-26 13:42:39 +0300

    upd 2

--- dialog/js/app.js
+++ dialog/js/app.js
@@ -1,8 +1,8 @@
 angular.module('ceoApp', ['ngWebSocket', 'frapontillo.gage','ui.odometer'])
 
 .controller('GetDataCtrl', function ($scope, $websocket) {
-    // var dataStream = $websocket('wss://whispering-retreat-6757.herokuapp.com');
-    var dataStream = $websocket('ws://localhost:5000/');
+    var dataStream = $websocket('wss://whispering-retreat-6757.herokuapp.com');
+    // var dataStream = $websocket('ws://localhost:5000/');
     dataStream.onMessage(function(message) {
 
         var data = JSON.parse(message.data);
