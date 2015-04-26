c9688205f167b53fc6baf293cc39e4bd7b0b3012
Carl-fridrix-zendolf-IV <social.neet@gmail.com>
2015-04-26 13:10:58 +0300

    update to angular logic

--- open/js/app.js
+++ open/js/app.js
@@ -1,19 +1,25 @@
-// Ionic Starter App
+angular.module('DialogApp', [])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+.controller('mainCtrl', function ($scope, $http) {
+    $scope.submit = function () {
+        if(localStorage.dialog_id != undefined){
+            $scope.result = 'Вы уже проголосовали!';
+            return false;
+        }
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        $http({
+            method: 'POST',
+            url: 'https://dialog-app.firebaseio.com/.json',
+            data: {
+                value: $scope.vote.value
+            }
+        })
+        .success(function (data) {
+            localStorage.dialog_id = Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
+            $scope.result = 'Спасибо за голос!';
+        })
+        .error(function (data) {
+            $scope.result = 'Что-то пошло нет так, попробуйте еще раз';
+        });
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
 })
